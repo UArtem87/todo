7d517668097d305b853ad38e7a4ea96557f7ba8b
Artem <usik887@ukr.net>
2026-01-23 01:22:06 +0200

    Updates

--- todo+-.d29bb997.js
+++ todo+-.d29bb997.js
@@ -714,96 +714,122 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"kyksZ":[function(require,module,exports,__globalThis) {
-const data = document.querySelector('.task-box__input');
-const save = document.querySelector('.task-box__btn');
-const todos = document.querySelector('.todos');
-data.focus();
-let toDoList = [];
-async function getList() {
-    loader.classList.remove('hidden');
-    const response = await fetch('https://696f53afa06046ce618642cd.mockapi.io/tasks');
-    const dataList = await response.json();
-    if (dataList) {
-        toDoList = dataList;
-        loader.classList.add('hidden');
-        console.log(toDoList);
-        renderToDo();
-    } else toDoList = [];
-}
-getList();
-async function setToDo() {
-    if (data.value === '') data.focus();
-    else {
-        const task = data.value;
-        await fetch('https://696f53afa06046ce618642cd.mockapi.io/tasks', {
-            method: 'POST',
+const tasksAdress = 'https://696f53afa06046ce618642cd.mockapi.io/tasks';
+const purchasesAdress = 'https://696f53afa06046ce618642cd.mockapi.io/purchases';
+let currentAdress = purchasesAdress;
+window.addEventListener('DOMContentLoaded', ()=>{
+    const header = document.querySelector('.header');
+    let data;
+    let todos;
+    let save;
+    let toDoList = [];
+    async function getList(adress) {
+        const activeBox = document.querySelector('.container-tasks:not(.hidden), .container-purchases:not(.hidden)');
+        let loader = activeBox.querySelector('.loader');
+        loader.classList.remove('hidden');
+        const response = await fetch(adress);
+        const dataList = await response.json();
+        if (dataList) {
+            toDoList = dataList;
+            loader.classList.add('hidden');
+            renderToDo();
+        } else toDoList = [];
+    }
+    async function setToDo(adress) {
+        if (data.value === '') data.focus();
+        else {
+            const task = data.value;
+            await fetch(adress, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    text: task
+                })
+            });
+            getList(adress);
+        }
+    }
+    function renderToDo() {
+        const activeBox = document.querySelector('.container-tasks:not(.hidden), .container-purchases:not(.hidden)');
+        data = activeBox.querySelector('.task-box__input');
+        save = activeBox.querySelector('.task-box__btn');
+        todos = activeBox.querySelector('.todos');
+        todos.innerHTML = '';
+        toDoList.forEach((todo, i)=>{
+            const task = `${todo.text.slice(0, 1).toUpperCase()}${todo.text.slice(1)}`;
+            const toDoBlock = document.createElement('div');
+            toDoBlock.setAttribute('id', todo.id);
+            toDoBlock.classList.add('todoblock');
+            const toDoItem = document.createElement('span');
+            toDoItem.classList.add('todos__item');
+            toDoItem.textContent = `${i + 1}. ${task}`;
+            let important = document.createElement('span');
+            important.classList.add('important');
+            important.innerHTML = todo.important ? "\u2605" : "\u2606";
+            if (todo.important) important.classList.add('active');
+            let del = document.createElement('span');
+            del.classList.add('del');
+            toDoBlock.append(toDoItem, important, del);
+            todos.append(toDoBlock);
+        });
+        save.onclick = ()=>setToDo(currentAdress);
+        data.onkeydown = (e)=>{
+            if (e.key === 'Enter') setToDo(currentAdress);
+        };
+        data.value = '';
+        data.focus();
+        todos.addEventListener('click', (e)=>{
+            const el = e.target;
+            const parent = el.closest('.todoblock');
+            const id = parent.getAttribute('id');
+            if (el.classList.contains('del')) deleteTask(currentAdress, id);
+            if (el.classList.contains('important')) {
+                el.classList.toggle('active');
+                const status = el.classList.contains('active');
+                el.innerHTML = status ? "\u2605" : "\u2606";
+                madeImportant(currentAdress, id, status);
+            }
+            if (el.classList.contains('todos__item')) el.classList.toggle('expanded');
+        });
+    }
+    async function deleteTask(adress, taskId) {
+        await fetch(`${adress}/${taskId}`, {
+            method: 'DELETE'
+        });
+        getList(adress);
+    }
+    async function madeImportant(adress, taskId, status) {
+        await fetch(`${adress}/${taskId}`, {
+            method: 'PUT',
             headers: {
-                'content-type': 'application/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                text: task
+                important: status
             })
         });
-        getList();
     }
-}
-function renderToDo() {
-    todos.innerHTML = '';
-    toDoList.forEach((todo, i)=>{
-        const task = `${todo.text.slice(0, 1).toUpperCase()}${todo.text.slice(1)}`;
-        const toDoBlock = document.createElement('div');
-        toDoBlock.setAttribute('id', todo.id);
-        toDoBlock.classList.add('todoblock');
-        const toDoItem = document.createElement('span');
-        toDoItem.classList.add('todos__item');
-        toDoItem.textContent = `${i + 1}. ${task}`;
-        const important = document.createElement('span');
-        important.classList.add('important');
-        if (todo.important) important.classList.add('active');
-        const status = important.classList.contains('active');
-        important.innerHTML = status ? "\u2605" : "\u2606";
-        const del = document.createElement('span');
-        del.classList.add('del');
-        toDoBlock.append(toDoItem);
-        toDoBlock.append(important);
-        toDoBlock.append(del);
-        todos.append(toDoBlock);
+    header.addEventListener('click', (e)=>{
+        const mainTitle = document.querySelector('.main-title');
+        const purchases = document.querySelector('.container-purchases');
+        const tasks = document.querySelector('.container-tasks');
+        if (e.target.classList.contains('task')) {
+            currentAdress = tasksAdress;
+            mainTitle.classList.add('hidden');
+            tasks.classList.remove('hidden');
+            purchases.classList.add('hidden');
+            getList(currentAdress);
+        }
+        if (e.target.classList.contains('purchases')) {
+            currentAdress = purchasesAdress;
+            mainTitle.classList.add('hidden');
+            tasks.classList.add('hidden');
+            purchases.classList.remove('hidden');
+            getList(currentAdress);
+        }
     });
-    data.value = '';
-    data.focus();
-}
-async function deleteTask(taskId) {
-    await fetch(`https://696f53afa06046ce618642cd.mockapi.io/tasks/${taskId}`, {
-        method: 'DELETE'
-    });
-    getList();
-}
-async function madeImportant(taskId, status) {
-    await fetch(`https://696f53afa06046ce618642cd.mockapi.io/tasks/${taskId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            important: status
-        })
-    });
-}
-data.addEventListener('keydown', (e)=>{
-    if (e.key === 'Enter') setToDo();
-});
-save.addEventListener('click', setToDo);
-todos.addEventListener('click', (e)=>{
-    const el = e.target;
-    const id = el.closest('.todoblock').getAttribute('id');
-    if (el.classList.contains('del')) deleteTask(id);
-    if (el.classList.contains('todos__item')) el.classList.toggle('expanded');
-    if (el.classList.contains('important')) {
-        el.classList.toggle('active');
-        const status = el.classList.contains('active');
-        el.innerHTML = status ? "\u2605" : "\u2606";
-        madeImportant(id, status);
-    }
 });
 
 },{}]},["7SvX3","kyksZ"], "kyksZ", "parcelRequire5af9", {})
